6c63b05d839b0126c32336bfa7166f3b1377f59e
rugbyprof <terry.griffin@msutexas.edu>
2023-09-25 16:59:00 -0500

    Pushed from Terrys-MacPro-15

---
+++ Assignments/P02/data_formatting.md
@@ -0,0 +1,73 @@
+When displaying database information from an SQLite database in Python, you have several options depending on your needs and the level of detail you want to present. Here are some common methods to display database information:
+
+1. **Pretty-Printed Tabular Output**: You can use libraries like `prettytable` or `tabulate` to format query results into a visually appealing tabular format. These libraries make it easy to display rows and columns in a structured way.
+
+   Example with `prettytable`:
+
+   ```python
+   from prettytable import PrettyTable
+   import sqlite3
+
+   conn = sqlite3.connect('your_database.db')
+   cursor = conn.cursor()
+
+   cursor.execute("SELECT * FROM your_table")
+   rows = cursor.fetchall()
+
+   table = PrettyTable()
+   table.field_names = [desc[0] for desc in cursor.description]
+   table.add_rows(rows)
+
+   print(table)
+   ```
+
+2. **Custom Formatting**: You can manually format the query results using Python's string formatting capabilities. This allows you to have full control over the display format.
+
+   Example:
+
+   ```python
+   conn = sqlite3.connect('your_database.db')
+   cursor = conn.cursor()
+
+   cursor.execute("SELECT * FROM your_table")
+   rows = cursor.fetchall()
+
+   for row in rows:
+       print(f"ID: {row[0]}, Name: {row[1]}, ...")
+   ```
+
+3. **JSON or CSV Export**: If you want to export data for further analysis or reporting, you can query the data and export it as JSON or CSV. Python has built-in support for both formats.
+
+   Example for CSV:
+
+   ```python
+   import csv
+   import sqlite3
+
+   conn = sqlite3.connect('your_database.db')
+   cursor = conn.cursor()
+
+   cursor.execute("SELECT * FROM your_table")
+   rows = cursor.fetchall()
+
+   with open('output.csv', 'w', newline='') as csvfile:
+       csvwriter = csv.writer(csvfile)
+       csvwriter.writerow([desc[0] for desc in cursor.description])
+       csvwriter.writerows(rows)
+   ```
+
+4. **Interactive Displays**: If you're working in a Jupyter Notebook or an environment that supports rich interactive displays, you can use libraries like `pandas` to display dataframes interactively.
+
+   Example with `pandas`:
+
+   ```python
+   import pandas as pd
+   import sqlite3
+
+   conn = sqlite3.connect('your_database.db')
+
+   df = pd.read_sql_query("SELECT * FROM your_table", conn)
+   df
+   ```
+
+The best method for displaying database information in SQLite using Python depends on your specific use case, whether you need interactive displays, further data processing, or simply a visually appealing tabular format. You can choose the method that best suits your needs and the environment in which you're working.
--- Assignments/P02/sqliteCRUD.py
+++ Assignments/P02/sqliteCRUD.py
@@ -1,11 +1,25 @@
 # Crud Class for Sqlite
 
 import sqlite3
-
+from prettytable import PrettyTable
+   
 class SQLiteCRUD:
-    def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name)
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
+
+    def __raw_results(self,results):
+        table = []
+        for row in results:
+            table.append(row[0])
+        return table     
+
+    def __formatted_results(self,results):
+        table = PrettyTable()
+        table.field_names = [desc[0] for desc in self.cursor.description]
+        table.add_rows(results)
+        return table
 
     def create_table(self, table_name, columns):
         try:
@@ -16,7 +30,40 @@ class SQLiteCRUD:
             print(f"Table '{table_name}' created successfully.")
         except sqlite3.Error as e:
             print(f"Error creating table: {e}")
+            
+    def show_tables(self,raw=True):
 
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        results = self.cursor.fetchall()
+
+        if not raw: 
+            return self.__formatted_results(results)
+        else:
+            return self.__raw_results(results)
+
+
+    def describe_table(self,table_name,raw=False):
+        self.cursor.execute(f"PRAGMA table_info({table_name});")
+        results = self.cursor.fetchall()
+        table = None
+        
+        if not raw:
+            table = self.__formatted_results(results)
+           
+        else:
+        
+            table= []
+
+            for column_info in results:
+                column_name = column_info[1]
+                data_type = column_info[2]
+                is_nullable = "NULL" if column_info[3] == 0 else "NOT NULL"
+                table.append({"column_name":column_name,"data_type":data_type,"isnull":is_nullable})
+                #print(f"Column Name: {column_name}, Data Type: {data_type}, Nullable: {is_nullable}")
+                
+        return table
+        
+            
     def insert_data(self, table_name, data):
         try:
             # Insert data into the table
@@ -65,6 +112,65 @@ class SQLiteCRUD:
     def close_connection(self):
         self.conn.close()
         print("Database connection closed.")
+        
+        
+    def formatted_print(self,table_name):
+        self.cursor.execute(f"SELECT * FROM {table_name};")
+        table_info = self.cursor.fetchall()
+        
+        table_info_list = []
+
+        table = PrettyTable()
+        table.field_names = [desc[0] for desc in self.cursor.description]
+        table.add_rows(table_info)
+
+        return table
+
+    def table_exists(self, table_name,db_path=None):
+        different_conn = False
+        if not db_path:
+            db_path = self.db_path
+            conn = sqlite3.connect(self.db_path)
+            cursor = conn.cursor()
+        else:
+            different_conn = True
+            conn = self.conn
+            cursor = self.conn.cursor()
+            
+        try:
+
+            # Query the sqlite_master table to check if the table exists
+            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+            result = cursor.fetchone()
+
+            # If result is not None, the table exists
+            return result is not None
+
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
+            return False
+
+        finally:
+            if different_conn:
+                conn.close()
+
+
+
+    def drop_table(self,table_name):
+        try:
+
+            # Drop the table if it exists
+            self.cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+
+            # Commit the changes
+            self.conn.commit()
+            return True
+
+        except sqlite3.Error as e:
+            print(f"SQLite error: {e}")
+            return False
+
+
 
 class FileSystem:
     def __init__(self):
@@ -97,31 +203,112 @@ class FileSystem:
         """
         pass
     
+import uuid
+
+def generate_uuid():
+    return str(uuid.uuid4())
 
 # Example usage:
 if __name__ == "__main__":
-    db_name = "my_database.db"
-    crud = SQLiteCRUD(db_name)
     
-    # Define table schema
-    table_name = "students"
-    columns = ["id INTEGER PRIMARY KEY", "name TEXT", "age INTEGER"]
+    crud1 = SQLiteCRUD("filesystem.sqlite")
+    crud2 = SQLiteCRUD("my_database.sqlite")
+    
+    print(crud1.table_exists("FileSystem3"))  
+    print(crud1.show_tables(raw=False))
+    
+    # cursor.execute("""
+    # CREATE TABLE FileSystem2 (
+    #     id INTEGER PRIMARY KEY,
+    #     pid INTEGER NOT NULL,
+    #     filename TEXT NOT NULL,
+    #     file_type TEXT NOT NULL,
+    #     file_size INTEGER,
+    #     owner TEXT NOT NULL,
+    #     group_name TEXT NOT NULL,
+    #     permissions TEXT NOT NULL,
+    #     modification_time DATETIME,
+    #     content BLOB,
+    #     hidden NUMBER 
+    # );
+    # """)
 
-    # Create table
-    crud.create_table(table_name, columns)
+    # # Inserting data into the FileSystem2 table
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (0, 'root', 'directory', NULL, 'root', 'admin', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
 
-    # Insert data
-    data = (1, "Alice", 25)
-    crud.insert_data(table_name, data)
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (1, 'home', 'directory', NULL, 'root', 'admin', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
 
-    # Read data
-    crud.read_data(table_name)
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (2, 'user1', 'directory', NULL, 'user1', 'users', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
 
-    # Update data
-    crud.update_data(table_name, "age", 26, "name", "Alice")
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (2, 'user2', 'directory', NULL, 'user2', 'users', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
 
-    # Delete data
-    crud.delete_data(table_name, "name", "Alice")
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (1, 'docs', 'directory', NULL, 'user1', 'users', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
+
+    # cursor.execute("""
+    #     INSERT INTO FileSystem2 (pid, filename, file_type, file_size, owner, group_name, permissions, modification_time, content, hidden)
+    #     VALUES (1, 'pictures', 'directory', NULL, 'user2', 'users', 'rwxr-xr-x', '2023-09-22 12:00:00', NULL, 0)
+    # """)
+
+    # # Commit the changes and close the connection
+    # conn.commit()
+    # conn.close()
+
+
+
+    # crud = SQLiteCRUD("filesystem.sqlite")
+    
+    # tables = crud.show_tables(True)
+    # print(tables)
+    
+    # table = crud.describe_table("FileSystem2",True)
+    # print(table)
+
+
+
+
+    # db_name = "my_database.sqlite"
+    # crud = SQLiteCRUD(db_name)
+    
+    # # Define table schema
+    # table_name = "students2"   
+    # columns = ["id TEXT", "name TEXT", "age INTEGER"]
+
+    # # # Create table
+    # crud.create_table(table_name, columns)
+
+    # # Insert data
+    # data = (generate_uuid(), "Alice", 25)
+    # crud.insert_data(table_name, data)
+    
+    # data = (generate_uuid(),"Bob", 23)
+    # crud.insert_data(table_name, data)
+    
+    # data = (generate_uuid(),"Charlie", 11)
+    # crud.insert_data(table_name, data)
+
+    # # Read data
+    # crud.read_data(table_name)
+
+    # # Update data
+    # crud.update_data(table_name, "age", 26, "name", "Alice")
+
+    # # Delete data
+    # crud.delete_data(table_name, "name", "Alice")
 
     # Close the database connection
-    crud.close_connection()
+    # crud.close_connection()
