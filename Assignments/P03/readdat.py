9df3f6a1b84fce132275aa624227ef1545644245
rugbyprof <terry.griffin@msutexas.edu>
2023-10-22 20:51:21 -0500

    added read data file

---
+++ Assignments/P03/readdat.py
@@ -0,0 +1,17 @@
+
+
+with open("datafile.dat") as f:
+    data = f.read().split("\n")
+
+
+
+for process in data:
+    if len(process) > 0:
+        parts = process.split(' ')
+        arrival = parts[0]
+        pid = parts[1]
+        priority = parts[2]
+        bursts = parts[3:]
+
+        print(f"{arrival}, {pid}, {priority} {bursts}")
+
