92f56577e72f8066c071d38f364cdbc772feddf5
Terry Griffin <terry.griffin@msutexas.edu>
2024-11-14 21:18:22 -0600

    Pushed from Terrys-MacBook-Air

--- Assignments/P03/cpu_jobs.py
+++ Assignments/P03/cpu_jobs.py
@@ -1,3 +1,29 @@
+"""
+Core Idea
+
+Call init to get a session_id and start_clock
+
+LOOP:
+Start iterating from start_clock and increment the clock by 1:
+    Get new jobs from the /job endpoint
+    /job will return a list of jobs that are available at the current clock time
+       - place new in new queue
+       - 1 tick later move to ready queue
+
+    - For jobs on a cpu decrement burst time for running CPU job
+        - If a jobs burst time gets to 0, move to wait queue
+        - get jobs next burst from the /burst endpoint
+    - For jobs in the ready queue (jobs waiting for cpu) increment wait time
+
+    - For jobs using an IO device decrement burst time for that running IO job 
+        - If a jobs burst time gets to 0, move to ready queue
+        - get jobs next burst from the /burst endpoint
+    - For jobs in the wait queue (waiting for IO device) increment wait time
+
+    - if burst is EXIT move job to terminated
+ 
+"""
+
 import requests
 import json 
 import os
